cc125f9957b5fd61040031a495a0d3edac21408a
pankajkarman <pankaj.kmr1990@gmail.com>
2021-07-26 21:34:26 +0530

    solved installation issue

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 with open('requirements.txt') as f:
     install_req = [req.strip() for req in f.read().split('\n')]
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
      name='pyvortex',
-     version='0.2.1',
+     version='0.2.3',
      description='Polar vortex edge detection using Nash criteria',
      long_description=long_description,
      long_description_content_type='text/markdown',
@@ -17,6 +17,7 @@ setup(
      author='Pankaj Kumar',
      author_email='pankaj.kmr1990@gmail.com',
      license='MIT',
+     packages=find_packages(),
      py_modules=['pyvortex'],
      install_requires=install_req,
      python_requires=">=3.6",
